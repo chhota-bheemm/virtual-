4856b4ffaf234cbd2a2537b3552c9e68e17e5393
chhota-bheemm <harsh27sahu2005@gmail.com>
2025-07-15 15:29:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let btn=document.querySelector("button")
 let text=document.querySelector("#text")
 let output=document.querySelector(".output")
 
-const Api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyA8BKMkmCPlT7eY7evtYByHruXGCKtrgac"
+const Api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyA8BKMkmCPlT7eY7evtYByHruXGCKtrgac"
 let fileDetails={
 
     mime_type:null,
